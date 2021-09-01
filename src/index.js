0436c9ef5a110384a0c553ad5aabc43804f60b0c
gammafer <gal.marcell96@gmail.com>
2021-09-01 17:48:40 +0200

    You can add new Todos now

--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ for (let i=0;i<10;i++){
     mainToDoScreen.insertBefore(toDoObject('test','test desc',new Date().toString(),'1'),addBtn);
 }
 
+const getToDoData=(form)=>{
+    //sends data to toDoObject.js
+    console.log(form);
+    const newToDO=toDoObject(form.title,form.description,form.date,form.priority);
+    mainToDoScreen.insertBefore(newToDO,addBtn);
+}
+
+export default getToDoData;
+
 
--- src/modules/addBtn.js
+++ src/modules/addBtn.js
@@ -1,6 +1,9 @@
+import getToDoData from "../index";
+
 const link=document.createElement('link');
 link.rel='stylesheet';
 link.href='./media/modules/addBtn.css';
+
 
 document.getElementsByTagName('HEAD')[0].appendChild(link);
 
@@ -8,12 +11,36 @@ const popupWindow=()=>{
     eListener.popup.hidden=false;
     console.log("hello");
 }
+const closePopup=()=>{
+    eListener.popup.hidden=true;
+}
+
+const addToDo=(e)=>{
+    //sends data to index.js
+    const formData = new FormData(e.target.parentNode);
+    const formProps = Object.fromEntries(formData);
+    getToDoData(formProps);
+    eListener.form.reset();
+    closePopup();
+}
 
 const eListener=(()=>{
+    //Adding values in IIFE
     const addBtn=document.getElementById('addBtn');
     const popup=document.getElementById('popupWindow');
     addBtn.addEventListener('click',popupWindow);
-    return {addBtn,popup};
+
+    const form=document.getElementById('popupForm')
+    
+    const sendForm=document.getElementById('sendForm');
+    sendForm.addEventListener('click',(e)=>{
+        e.preventDefault();
+        addToDo(e)});
+    
+    const closeBtn=document.getElementById('closePopup');
+    closeBtn.addEventListener('click',closePopup);
+
+    return {addBtn,popup,form};
 })();
 
 
